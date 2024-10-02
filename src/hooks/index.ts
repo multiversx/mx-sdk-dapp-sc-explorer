a669ab07cc90da46275f9f17e84c0eb4d7bbb9ff
Radu Mojic <radu.mojic@multiversx.com>
2024-10-02 14:40:26 +0300

    split InteractionForm functionality from Modal

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,7 @@ export * from './provider';
 export * from './useGetAccountTokens';
 export * from './useGetTransaction';
 export * from './useGetTransactionCost';
+export * from './useGetTransactionCostDetails';
 export * from './useQueryContract';
 export * from './useTrackTransaction';
 export * from './useUpdateDeployedContractDetails';
