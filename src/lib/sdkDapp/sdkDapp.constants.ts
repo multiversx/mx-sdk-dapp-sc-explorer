300b41da524030624e0a6c977dc42430f5de3150
Radu Mojic <radu.mojic@multiversx.com>
2025-12-03 19:10:21 +0200

    Migrate to pnpm, update imports

--- src/lib/sdkDapp/sdkDapp.constants.ts
+++ src/lib/sdkDapp/sdkDapp.constants.ts
@@ -4,6 +4,6 @@ export {
   VERSION,
   GAS_PER_DATA_BYTE,
   GAS_PRICE_MODIFIER,
-  EXTRA_GAS_LIMIT_GUARDED_TX,
-  fallbackNetworkConfigurations
-} from '@multiversx/sdk-dapp/out/constants';
+  EXTRA_GAS_LIMIT_GUARDED_TX
+} from '@multiversx/sdk-dapp/out/constants/mvx.constants';
+export { fallbackNetworkConfigurations } from '@multiversx/sdk-dapp/out/constants/network.constants';
